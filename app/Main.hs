995d5da146f94a5cf46f3b779dbc4a39c2c96f7a
Jamie Bayne <jamiebayne@hotmail.com>
2022-05-06 03:06:03 +0100

    Resolve possible failures or input parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
+
+import Control.Monad(forM_)
+import Data.Bitraversable (bimapM)
 import Data.Either
 import System.Environment (getArgs)
-import Data.Attoparsec.ByteString (parseOnly)
-import Data.ByteString.Char8 (pack)
 import qualified Data.ByteString.Lazy.Char8 as LC8
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.HashSet as S
+
 import ParseArguments
+import Result
+import qualified ColSpec
+import qualified Regex as RE
+
+fieldSeparator = ','
+recordSeparators = "\n\r"
 
 main :: IO ()
 main = do
@@ -18,3 +29,26 @@ main = do
         (arg, Left err) -> putStrLn $ "Error parsing argument `" ++ arg ++ "': " ++ err
         _ -> return ())
 
+  else do
+    let columnNames = C8.split fieldSeparator $ LC8.toStrict firstLine
+        numColumns = length columnNames
+
+        resolveColspecs = fmap allSucceed . mapM (ColSpec.resolve columnNames (numColumns - 1))
+        resolveComparators = fmap bothSucceed . bimapM resolveColspecs RE.compile
+
+        (outputColumns, comparators) = partitionEithers $ rights parseResults
+
+    resolvedOutputs <- resolveColspecs $ concat outputColumns
+    resolvedComparators <- sequence <$> mapM resolveComparators comparators :: IO (Result [([Int], RE.Regex)])
+
+    case resolvedOutputs >> resolvedComparators of
+       Left err -> print err
+       otherwise -> process (S.fromList resolvedOutputs) resolvedComparators remainingLines
+    print resolvedOutputs
+
+--process :: S.HashSet Int -> [([Int], RE.Regex)] -> LC8.ByteString -> IO ()
+--process outputIndices comparators input =
+--  forM_ (LC8.splitWith (`elem` recordSeparators) input) (\line -> do
+--    let lineS = LC8.toStrict line
+--        fields = C8.split fieldSeparator lineS
+--                                                                                                )
