df70a09c1b7274689ac5b624dbccbfb697bcb844
Jamie Bayne <jamiebayne@hotmail.com>
2022-05-06 21:33:08 +0100

    Implement filtering

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,14 @@ module Main where
 
 import Control.Monad(forM_)
 import Data.Bitraversable (bimapM)
+import Data.List (foldl', sortBy)
 import Data.Either
 import System.Environment (getArgs)
+import Text.Regex.Base (matchTest)
 import qualified Data.ByteString.Lazy.Char8 as LC8
 import qualified Data.ByteString.Char8 as C8
-import qualified Data.HashSet as S
+import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 
 import ParseArguments
 import Result
@@ -36,19 +39,30 @@ main = do
         resolveColspecs = fmap allSucceed . mapM (ColSpec.resolve columnNames (numColumns - 1))
         resolveComparators = fmap bothSucceed . bimapM resolveColspecs RE.compile
 
-        (outputColumns, comparators) = partitionEithers $ rights parseResults
+        (outputColumnSpecs, comparatorSpecs) = partitionEithers $ rights parseResults
 
-    resolvedOutputs <- resolveColspecs $ concat outputColumns
-    resolvedComparators <- sequence <$> mapM resolveComparators comparators :: IO (Result [([Int], RE.Regex)])
+    resolvedOutputColumns <- resolveColspecs $ concat outputColumnSpecs
+    resolvedComparators <- sequence <$> mapM resolveComparators comparatorSpecs :: IO (Result [([Int], RE.Regex)])
 
-    case resolvedOutputs >> resolvedComparators of
-       Left err -> print err
-       otherwise -> process (S.fromList resolvedOutputs) resolvedComparators remainingLines
-    print resolvedOutputs
+    case (do
+      outputColumns <- S.fromList <$> resolvedOutputColumns
+      comparators <- comparatorsByColumn <$> resolvedComparators
+      let inputLines = LC8.splitWith (`elem` recordSeparators) remainingLines
+      return $ processRecords outputColumns comparators inputLines
+      ) of Left err -> putStrLn err
+           Right lines -> mapM_ LC8.putStrLn lines
 
---process :: S.HashSet Int -> [([Int], RE.Regex)] -> LC8.ByteString -> IO ()
---process outputIndices comparators input =
---  forM_ (LC8.splitWith (`elem` recordSeparators) input) (\line -> do
---    let lineS = LC8.toStrict line
---        fields = C8.split fieldSeparator lineS
---                                                                                                )
+comparatorsByColumn :: [([Int], RE.Regex)] -> M.Map Int [RE.Regex]
+comparatorsByColumn = foldl' addToMap M.empty
+  where addToMap m (ix, re) = foldl' (\m i -> M.insertWith (++) i [re] m) m ix
+
+processRecords :: S.Set Int -> M.Map Int [RE.Regex]  -> [LC8.ByteString] -> [LC8.ByteString]
+processRecords outputIndices comparators = filter recordMatches
+
+  where recordMatches = or . map fieldMatches . toFields
+
+        toFields = zip [1..] . map LC8.toStrict . LC8.split fieldSeparator
+
+        fieldMatches :: (Int, C8.ByteString) -> Bool
+        fieldMatches (i, field) = let regexes = M.findWithDefault [] i comparators
+                                  in or $ matchTest <$> regexes <*> pure field
